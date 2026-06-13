441b54e1cb8316fb789c8828a47d2562ed197857
Visal-2406 <visalvs2406@gmail.com>
2026-06-13 21:54:48 +0530

    create database and login

---
+++ interview-ai/src/routes/auth.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const authRouter = express.Router();
+const authController = require("../controllers/auth.controller");
+
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post("/register",authController.registerUserController);
+
+
+/** 
+ * @route POST /api/auth/login
+ * @description Login a user with email and password
+ * @access Public
+ */
+authRouter.post("/login",authController.loginUserController);
+
+
+module.exports = authRouter;
